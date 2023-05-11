dc0b54bd4ed9c3714580ebbf44570fc335715296
Michael Loughman <mcloughman@gmail.com>
2023-05-11 07:42:55 -0700

    Correct typos

--- index.js
+++ index.js
@@ -4,42 +4,39 @@
 1 kilogram = 2.204 pound
 */
 
-const input = document.querySelector("input")
-const btn = document.querySelector("button")
-const lengthP = document.getElementById("length")
-const volumeP = document.getElementById("volume")
-const massP = document.getElementById("mass")
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const lengthP = document.getElementById("length");
+const volumeP = document.getElementById("volume");
+const massP = document.getElementById("mass");
 
-btn.addEventListener("click", function() {
-    const numToConvert = Number(input.value)
-    const lengthObj = lengthConverter(numToConvert)
-    const volumeObj = volumeConverter(numToConvert)
-    const massObj = massConverter(numToConvert)
-    lengthP.textContent = `${numToConvert} meters = ${lengthObj.feet} feet | ${numToConvert} feet = ${lengthObj.meters} meters`
-    volumeP.textContent = `${numToConvert} liters = ${volumeObj.gallons} gallons | ${numToConvert} gallons = ${volumeObj.liters} liters`
-    massP.textContent = `${numToConvert} kilos = ${massObj.pounds} gpounds | ${numToConvert} pounds= ${massObj.kilos} kilos`
-    
-    
-    
-})
+btn.addEventListener("click", function () {
+  const numToConvert = Number(input.value);
+  const lengthObj = lengthConverter(numToConvert);
+  const volumeObj = volumeConverter(numToConvert);
+  const massObj = massConverter(numToConvert);
+  lengthP.textContent = `${numToConvert} meters = ${lengthObj.feet} feet | ${numToConvert} feet = ${lengthObj.meters} meters`;
+  volumeP.textContent = `${numToConvert} liters = ${volumeObj.gallons} gallons | ${numToConvert} gallons = ${volumeObj.liters} liters`;
+  massP.textContent = `${numToConvert} kilos = ${massObj.pounds} pounds | ${numToConvert} pounds= ${massObj.kilos} kilos`;
+});
 
 function lengthConverter(num) {
-    const feet = (num * 3.281).toFixed(3)
-    const meters = (num / 3.281).toFixed(3)
-    const lengthObj = {feet, meters}
-    return lengthObj
+  const feet = (num * 3.281).toFixed(3);
+  const meters = (num / 3.281).toFixed(3);
+  const lengthObj = { feet, meters };
+  return lengthObj;
 }
 
 function volumeConverter(num) {
-    const gallons = (num * .264).toFixed(3)
-    const liters = (num / .264).toFixed(3)
-    const volumeObj = {gallons, liters}
-    return volumeObj
+  const gallons = (num * 0.264).toFixed(3);
+  const liters = (num / 0.264).toFixed(3);
+  const volumeObj = { gallons, liters };
+  return volumeObj;
 }
 
-function massConverter(num)  {
-    const pounds = (num * 2.204).toFixed(3)
-    const kilos = (num / 2.204).toFixed(3)
-    const massObj = {pounds, kilos}
-    return massObj
+function massConverter(num) {
+  const pounds = (num * 2.204).toFixed(3);
+  const kilos = (num / 2.204).toFixed(3);
+  const massObj = { pounds, kilos };
+  return massObj;
 }
